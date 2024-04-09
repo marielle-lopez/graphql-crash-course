45bc9c7d1be187be91f9c972fa1bc4cf317823ad
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-04-09 23:34:41 +1000

    add resolvers, setup server, and start server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from '../schema';
+import db from '../_db';
+
+const resolvers = {
+  Query: {
+    games: () => {
+      return db.games;
+    },
+    reviews: () => {
+      return db.reviews;
+    },
+    authors: () => {
+      return db.authors;
+    },
+  },
+};
+
+// server setup
+const server = new ApolloServer({
+  // typeDefs property
+  // type definitions; descriptions of data types and their relationships with other data types
+  typeDefs,
+
+  // resolvers property
+  // resolver functions that determine how queries are responded to for different data
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server is ready at port ${url}`);
